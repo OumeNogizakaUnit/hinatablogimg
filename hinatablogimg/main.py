9597bba7f2ff5fe4aab8d4c6e841624b6ce8e328
KAWAI Shun <shun@osstech.co.jp>
2021-01-12 17:52:53 +0900

    ログレベルとログファイルをオプションで指定出来るようになった

--- hinatablogimg/main.py
+++ hinatablogimg/main.py
@@ -9,8 +9,7 @@ from pathlib import Path
 
 from hinatablogimg import __name__ as modname
 from hinatablogimg import __version__ as modversion
-from hinatablogimg import (HINATADIR,
-                           LOGCONFPATH)
+from hinatablogimg import LOGCONFPATH
 from hinatablogimg.utils import fetch_one_page
 
 
@@ -25,17 +24,34 @@ from hinatablogimg.utils import fetch_one_page
               default=3,
               show_default=True,
               help='探索終了ページ')
+@click.option('--logpath',
+              type=click.Path(exists=False),
+              default='./log/hinatablogimg.log',
+              show_default=True,
+              help='ログファイル出力先パス')
+@click.option('--loglevel',
+              default='INFO',
+              type=click.Choice(['DEBUG', 'INFO',
+                                 'WARNING', 'ERROR', 'CRITICAL'],
+                                case_sensitive=False),
+              show_default=True,
+              help='ログレベル')
 @click.argument('savedir',
                 type=click.Path(exists=True))
 @click.version_option(modversion, prog_name=modname)
-def main(startpage, endpage, savedir):
+def main(startpage,
+         endpage,
+         logpath,
+         loglevel,
+         savedir):
     '''
 Argument:
 
     SAVEDIR\t\t探索画像保存先パス
     '''
     load_dotenv()
-    logger = load_logger()
+    logger = load_logger(logpath=logpath)
+    logger.setLevel(loglevel)
     for i in range(startpage, endpage+1):
         logger.debug(f"{i}ページ目の処理開始")
         try:
@@ -45,11 +61,16 @@ Argument:
             sys.exit(1)
 
 
-def load_logger():
-    logpath = Path(LOGCONFPATH)
-    if logpath.exists():
-        with logpath.open() as fd:
+def load_logger(logpath=None):
+    logconfpath = Path(LOGCONFPATH)
+    if logconfpath.exists():
+        with logconfpath.open() as fd:
             confdata = yaml.safe_load(fd)
+            if logpath:
+                try:
+                    confdata['handlers']['filehandler']['filename'] = logpath
+                except KeyError as error:
+                    sys.stderr.write(error)
             logging.config.dictConfig(confdata)
     return logging.getLogger(modname)
 
